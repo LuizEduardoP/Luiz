5eee0a176f04999e51ab1d00058083966a9737e3
Luiz Eduardo <luiz.padilha1@aluno.sc.senac.br>
2022-08-26 20:35:41 -0300

    FIZ O MELHOR

---
+++ src/githubaula1/GitHubAula1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubaula1;
+
+/**
+ *
+ * @author luiz.padilha1
+ */
+public class GitHubAula1 {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORlD");
+        
+        
+        
+        
+        
+    }
+    
+}
